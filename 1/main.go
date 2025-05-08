16d434d2985da1a763c23c31d3b985d9210e3086
José <joseantunes2001@gmail.com>
2025-05-08 16:51:11 -0300

    aula 1 e 2

--- 1/main.go
+++ 1/main.go
@@ -1,8 +1,5 @@
 package main
 
-const msg = "Hello world" // constante
-var b bool                // variavel boolean (default false)
-
 func main() {
-	println(b)
+	println(msg)
 }
---
+++ 2/main.go
@@ -0,0 +1,8 @@
+package main
+
+const msg = "Hello world" // constante
+var b bool                // variavel boolean (default false)
+
+func main() {
+	println(b)
+}
