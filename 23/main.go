a8b447347c0a8a142a045d4d62d80ef90fffe637
Jose <joseantunes2001@gmail.com>
2025-05-21 19:13:23 -0300

    aula 25 - for

---
+++ 23/main.go
@@ -0,0 +1,37 @@
+package main
+
+// em go para loops temos apenas o for
+func main() {
+	// for convencional
+	// for i := 0; i < 10; i++ {
+	// 	println(i)
+	// }
+
+	// for com range
+	// numeros := []string{"Um", "Dois", "Três"}
+	// for idx, value := range numeros {
+	// 	println(idx, value)
+	// }
+
+	// ocultando o indice
+	// for _, value := range numeros {
+	// 	println(value)
+	// }
+
+	// ocultando o valor
+	// for idx := range numeros {
+	// 	println(idx)
+	// }
+
+	// for estilo while
+	// i := 0
+	// for i < 10 {
+	// 	println(i)
+	// 	i++
+	// }
+
+	// loop infinito
+	// for {
+	// 	println("Hello world!")
+	// }
+}
