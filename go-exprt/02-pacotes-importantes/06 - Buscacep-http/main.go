6c9836d1f778e974864efc415444d3a821cf1aa2
José <joseantunes2001@gmail.com>
2025-10-08 12:59:11 -0300

    aula 34 - manipulando headers

--- go-exprt/02-pacotes-importantes/06 - Buscacep-http/main.go
+++ go-exprt/02-pacotes-importantes/06 - Buscacep-http/main.go
@@ -9,10 +9,22 @@ func main() {
 	// http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 	// 	w.Write([]byte("Hello world!"))
 	// })
-	http.HandleFunc("/", BuscaCEP)
+	http.HandleFunc("/", BuscaCEPHandler)
 	http.ListenAndServe(":8081", nil) // rodando o http server
 }
 
-func BuscaCEP(w http.ResponseWriter, r *http.Request) {
+func BuscaCEPHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound) // envia uma resposta http com um status code (nesse caso 404 not found)
+		// http.NotFound(w, r)
+		return
+	}
+	cepParam := r.URL.Query().Get("cep") // recuperando o valor de um query param (?cep=123213213)
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json") // altera o valor de um header
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello world!"))
 }
