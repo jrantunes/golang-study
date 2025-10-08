dabb3a2f47d7d1d7fe9c478a7b88369b199b9292
José <joseantunes2001@gmail.com>
2025-10-08 12:20:45 -0300

    aula 32 - buscacep

--- go-exprt/02-pacotes-importantes/05 - Buscacep/main.go
+++ go-exprt/02-pacotes-importantes/05 - Buscacep/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
+// Gerado com https://mholt.github.io/json-to-go/
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
 func main() {
-	for _, url := range os.Args[1:] { // go run main.go ola (url -> ola)
-		req, err := http.Get(url)
+	for _, cep := range os.Args[1:] { // go run main.go ola (url -> ola)
+		req, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Erro ao fazer a requisição: %v\n", err) // Fprintf - manda o valor para um lugar especifico - Fprintf(lugar, valor)
 		}
@@ -18,5 +36,25 @@ func main() {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Erro ao ler a resposta: %v\n", err)
 		}
+		var data ViaCEP
+		err = json.Unmarshal(res, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer o parse da resposta: %v\n", err)
+		}
+		file, err := os.Create("./cidade.txt")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao criar o arquivo: %v\n", err)
+		}
+		defer file.Close()
+		// Sprintf - joga/retorna a saida para outro lugar
+		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", data.Cep, data.Localidade, data.Uf))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao escrever no arquivo: %v\n", err)
+		}
+		fmt.Println("Arquivo criado com sucesso")
+		fmt.Println("Cidade: ", data.Localidade)
 	}
 }
+
+// buildado com: go build -o cep main.go
+// executado com ./cep 66640005
