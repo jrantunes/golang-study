e91eba6c5a935a46140352c9d7e65aae48756492
Jose <joseantunes2001@gmail.com>
2025-10-16 19:10:50 -0300

    aula 37 - ServeMux

--- go-exprt/02-pacotes-importantes/07 - ServeMux/main.go
+++ go-exprt/02-pacotes-importantes/07 - ServeMux/main.go
@@ -11,11 +11,29 @@ func main() {
 	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // attachando um handler no mux
 	// 	w.Write([]byte("Hello World!"))
 	// })
-	// handler implementa uma interface que torna obrigatório o uso de um metdodo ServeHTTP
 	mux.HandleFunc("/", HomeHandler)
+	// handler implementa uma interface que torna obrigatório o uso de um metodo ServeHTTP
+	//ex:
+	mux.Handle("/blog", blog{}) // uma struct para cada endpoint | essa forma é mais customizavel
+	// ex customização:
+	mux.Handle("/blog2", blogWithTitle{title: "Hello blog2"})
 	http.ListenAndServe(":8081", mux) // subindo um webserver utilizando o mux criado acima
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ASDHFASHFASFHA"))
+	w.Write([]byte("Hello home"))
+}
+
+type blog struct{}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello blog"))
+}
+
+type blogWithTitle struct {
+	title string
+}
+
+func (b blogWithTitle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(b.title))
 }
