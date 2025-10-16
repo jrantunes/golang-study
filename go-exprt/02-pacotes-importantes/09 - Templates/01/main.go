73dcc901984b6da699950feb173d5d34ddd6d512
Jose <joseantunes2001@gmail.com>
2025-10-16 19:32:57 -0300

    aula 39 - Iniciando com templates

---
+++ go-exprt/02-pacotes-importantes/09 - Templates/01/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+func main() {
+	curso := Curso{"Go", 60}
+	tmp := template.New("CursoTemplate")
+	tmp, _ = tmp.Parse("Curso: {{.Nome}}, Carga horária: {{.CargaHoraria}}")
+	err := tmp.Execute(os.Stdout, curso) // manda o resultado para Stdout
+	if err != nil {
+		panic(err)
+	}
+}
