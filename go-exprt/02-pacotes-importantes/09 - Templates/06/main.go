ebd74c61330b85603eb3f55a876dcd87dd3adedb
Jose <joseantunes2001@gmail.com>
2025-10-16 20:54:55 -0300

    aula 44 - Mapeando funções nos templates

---
+++ go-exprt/02-pacotes-importantes/09 - Templates/06/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+func main() {
+	templates := []string{
+		"header.html",
+		"content.html",
+		"footer.html",
+	}
+	t := template.New("content.html")
+	t.Funcs(template.FuncMap{"ToUpper": ToUpper})
+	// t.Funcs(template.FuncMap{"ToUpper": strings.ToUpper})
+	t = template.Must(t.ParseFiles(templates...))
+	err := t.Execute(os.Stdout, Cursos{
+		{"Go", 40},
+		{"Java", 20},
+		{"Python", 30},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
